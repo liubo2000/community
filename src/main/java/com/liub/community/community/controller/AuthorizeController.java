38623be5776b78f4f5ced467bc30817bd0540a8f
Codearcher <17434520252@.qq.com>
2020-09-27 10:05:10 +0800

    github config from code to properties

--- src/main/java/com/liub/community/community/controller/AuthorizeController.java
+++ src/main/java/com/liub/community/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.liub.community.community.dto.AccessTokenDTO;
 import com.liub.community.community.provider.GithubProvider;
 import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,15 +13,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private  String clientSecret;
+    @Value("${github.redirect.url}")
+    private String redirectUrl;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
                            @RequestParam(name="state") String state){
     AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setClient_id("c8d51d6dbcc5633129a9");
-        accessTokenDTO.setClient_secret("2869947536a24223ddc2b033615cfbd9fffce354");
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setRedirect_uri(redirectUrl);
         accessTokenDTO.setState(state);
         githubProvider.getAccessToken(new AccessTokenDTO());
         return "index";
--- src/main/java/com/liub/community/community/provider/GithubProvider.java
+++ src/main/java/com/liub/community/community/provider/GithubProvider.java
@@ -1,7 +1,9 @@
 package com.liub.community.community.provider;
 
 
+import com.alibaba.fastjson.JSON;
 import com.liub.community.community.dto.AccessTokenDTO;
+import com.liub.community.community.dto.GithubUser;
 import okhttp3.*;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +12,9 @@ import java.io.IOException;
 @Component
 public class GithubProvider {
     public String getAccessToken(AccessTokenDTO accessTokenDTO) {
-        MediaType JSON = MediaType.get("application/json; charset=utf-8");
-
+        MediaType mediaType=MediaType.Companion.parse("application/json;charset=utf-8");
         OkHttpClient client = new OkHttpClient();
-        RequestBody body = RequestBody.create(JSON, accessTokenDTO.);
+        RequestBody body = RequestBody.Companion.create(  JSON.toJSONString(accessTokenDTO),mediaType );
         Request request = new Request.Builder()
                 .url("https://github.com/login/oauth/access_token")
                 .post(body)
@@ -25,5 +26,22 @@ public class GithubProvider {
         }catch(IOException e){
 
         }
+        return null;
+    }
+
+    public GithubUser getUser(String accessToken ){
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                    .url("https://api.github.com/user?access_token="+accessToken)
+                .build();
+
+        try {
+                Response response =client.newCall(request).execute();
+            String string=response.body().string();
+            GithubUser githubUser = JSON.parseObject(string, GithubUser.class);
+            return  githubUser;
+        } catch (IOException e) {
+        }
+        return null;
     }
 }
